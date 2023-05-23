7075ccf15ff041f0c28b8e2ae39d9a09748542c9
JohnJohn21121 <john.abreu21121@gmail.com>
2023-05-23 15:45:25 -0300

    initializing

---
+++ COLORS360Studio/src/main/java/com/johnjohn21121/COLORS360Studio/Colors360StudioApplication.java
@@ -0,0 +1,13 @@
+package com.johnjohn21121.COLORS360Studio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Colors360StudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Colors360StudioApplication.class, args);
+	}
+
+}
---
+++ COLORS360Studio/src/test/java/com/johnjohn21121/COLORS360Studio/Colors360StudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnjohn21121.COLORS360Studio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Colors360StudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
